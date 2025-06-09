082cb297ef5f87a23a2d53942e0ea69bd2f5b21f
Cayky153 <antonio.cayky987@gmail.com>
2025-06-08 23:25:51 -0300

    API feita no index.js, podendo ver a lista,adicionar e deletar, junto do banco de dados rudimentar na connect.js por meio de sql3

---
+++ connect.js
@@ -0,0 +1,29 @@
+
+import sqlite3 from "sqlite3";
+const sql3 = sqlite3.verbose();
+
+const DB = new sql3.Database('./mydata.db',sqlite3.OPEN_READWRITE,connected);
+
+
+function connected(err){   
+    if(err){
+        return new Error();
+    }
+    console.log("Connected to the DB or SQLite Db does already exist")
+}
+
+let sql= `CREATE TABLE IF NOT EXISTS enemies(
+    enemy_id INTEGER PRIMARY KEY,
+    enemy_name TEXT NOT NULL,
+    enemy_reason TEXT NOT NULL
+
+) `;
+DB.run(sql,[],(err)=>{
+    //callback function
+    if(err){ console.log(`error creating enemies table`);
+    return;
+    }
+    console.log('CREATED TABLE')
+});
+
+export {DB};

04fb4fdbeb278aa24f39a9a061a685bd63be547a
Cayky153 <antonio.cayky987@gmail.com>
2025-06-26 20:39:46 -0300

    cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import {DB} from './connect.js';
 
 import express from 'express'
 import bodyParser from "body-parser";
+import cors from 'cors';
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.status(200);

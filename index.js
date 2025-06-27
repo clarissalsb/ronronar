9af28289a3e3b1b619606a82ef07e698cf5e2aaf
Cayky153 <antonio.cayky987@gmail.com>
2025-06-27 04:29:47 -0300

    Alteraçoes feitas no Cors

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
+
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500'); // igual ao seu corsOptions
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 app.get('/',(req,res)=>{
     res.status(200);
     res.send('Serviço de usuários está online');
